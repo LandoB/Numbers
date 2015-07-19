858b0c84746ca290d8aa6011230ff93c4fa226be
LandoB <jrolandobernal@gmail.com>
2015-07-18 23:03:31 -0500

    Initial Commit

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Numbers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Please enter a positive integer: ");
+			int upperLimit = int.Parse (Console.ReadLine());
+
+				List<int> evenIntegers = new List<int>();
+
+				for (var i = 1; i < upperLimit; i++)
+				{
+					if (i % 2 == 0)
+					{
+						evenIntegers.Add(i);
+						Console.WriteLine(i);
+					}
+						
+				}
+		}
+	}
+}
