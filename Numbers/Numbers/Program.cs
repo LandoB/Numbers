88e7247e79f1ae6238f72d4a9c7d148f3a01124a
LandoB <jrolandobernal@gmail.com>
2015-07-19 01:30:22 -0500

    AddArrayOfEvenNumbers assigment completed

--- Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Program.cs
@@ -10,20 +10,29 @@ namespace ArrayAdding
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Please enter a positive integer: ");
+			Console.WriteLine (); // I just need a blank space here
+
 			int upperLimit = int.Parse(Console.ReadLine());
+
+			Console.WriteLine (); // I just need a blank space here
+			Console.WriteLine ("Your even numbers are: "); // Listing all even numbers in the list
 
 			List<int> evenIntegers = new List<int>();
 
+			int sum = 0;
 			for (var i = 1; i < upperLimit; i++)
 			{
 				if (i % 2 == 0)
 				{
 					evenIntegers.Add(i);
 					Console.WriteLine(i);
+					sum += i;
 				}
 			}
 
-
+			Console.WriteLine (); // I just need a blank space here
+			Console.WriteLine("Sum of even numbers in array is: ");
+			Console.WriteLine (sum);
 		}
 	}
 }
